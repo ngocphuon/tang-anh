c74f9e4fc4ae445bb78662c12e036b18d8ae15bf
ngocphuon <86772990+ngocphuon@users.noreply.github.com>
2021-07-01 16:26:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
     introTitle: 'Bbi à!',
-    introDesc: `Em đã trằn trọc suốt đêm qua đặt ra một câu hỏi 👉👈 `,
+    introDesc: `Em đã trằn trọc suốt đêm qua đặt ra một câu hỏi `,
     btnIntro: 'Sao em',
     title: 'Anh có iu em khônggggg',
-    desc: '✨Anh không trả lời em anh là con 🐽✨ ',
+    desc: '✨Anh không trả lời em anh là con ',
     btnYes: 'Khonggggg',
     btnNo: 'Coaaaaa',
     question:'Trên thế giới này có tỷ tỷ con 🐽 mà sao anh lại chọn như thế 🥺',
@@ -12,5 +12,5 @@ const CONFIG = {
     mess: 'Em biết mà em hiểu mà',
     messDesc: 'Đồ dối gian.',
     btnAccept: ':3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/ngocphuonnnnn
 }
